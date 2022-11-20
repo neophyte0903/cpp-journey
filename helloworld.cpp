e69d65f5cce3c1c722e0903bfb6c26c9dca1e2fc
Gagandeep Singh <100983473+neophyte0903@users.noreply.github.com>
2022-11-20 19:43:56 +0530

    added helloworld

---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   cout << "hello world" <<endl;
+   cout << "i love pizza" << '\n';
+   cout << "it's really good";
+    //comment are used for notes 
+    /*this is
+    a multiline
+    comment  */
+    return 0;
+}
