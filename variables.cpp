e644bbb2595b258eed10b168716fad455f07e4a1
Gagandeep Singh <100983473+neophyte0903@users.noreply.github.com>
2022-11-20 19:58:11 +0530

    variables and data types

---
+++ variables.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    /* code */
+    //integer
+    int age = 19;
+    int year = 2022;
+
+    //float
+    float currency = 25.56;
+
+    // double
+    double validity = 456.99998;
+    double pi = 3.1415826;
+
+    // char
+    char a='a';
+    char ini='g';
+
+    // string
+    string red="hello there ";
+
+
+
+    cout << age <<" "<< year << endl;
+    cout << a <<" "<< ini <<" "<< red << endl;
+    cout << pi <<" "<< validity << endl;
+    return 0;
+}
