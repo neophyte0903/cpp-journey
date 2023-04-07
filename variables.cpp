33e3b9e8c075226b65611d39360203f5022b1b90
gagandeep singh <gagandeep.21gcebaids001@galgotiacollege.edu>
2023-04-07 14:45:04 +0530

    "variables cpp added"

---
+++ variables.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    
+    int a;
+    float b;
+    char c;
+    double d=3.45567;
+    int arr[]={1,2,3,4,5};
+    string s="hello";
+
+
+    cin>>a>>b;
+
+    printf("%d\n%f\n%d",a,b,arr[0]);
+
+    return 0;
+}
